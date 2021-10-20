989575c08e4538bf49753509f64dd010e08ae5a7
Anirban <85803381+anirbandsharma@users.noreply.github.com>
2021-10-20 15:56:51 +0530

    update2

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ tl.fromTo('.slide', {y:0}, {y:-400});
 tl2.fromTo(
     '.logo', 
     {scale:5, boder: "solid 2px hsla(0, 0%, 0%, 1)"},
-    {scale:1, top:"1rem", left:"5rem", x: '50%', y: '50%', border: "solid 2px hsla(0, 0%, 0%, 0)", padding: "0"}
+    {scale:1, top:"0.6rem", left:"5rem", x: '50%', y: '50%', border: "solid 2px hsla(0, 0%, 0%, 0)", padding: "0"}
     );
 
-tlN.fromTo('.nav', {background: "transparent", color: "white"}, {background:"white", color: "black"});
+tlN.fromTo('.nav', {background: "transparent", color: "white"}, {background:"#ffffffea", color: "black"});
