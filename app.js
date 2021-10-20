f638472fb7ff49f1a9370e92317b2ea4acf9888f
Anirban <85803381+anirbandsharma@users.noreply.github.com>
2021-10-20 12:30:40 +0530

    update

---
+++ app.js
@@ -0,0 +1,48 @@
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "80%",
+        scrub: 1,
+    },
+});
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "80%",
+        scrub: 1,
+    },
+});
+
+let tlN = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "80%",
+        scrub: 1,
+    },
+});
+
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "300%",
+        scrub: 1,
+        pin: true,
+        pinSpacing: false,
+    },
+});
+
+tl.fromTo('.slide', {y:0}, {y:-400});
+
+tl2.fromTo(
+    '.logo', 
+    {scale:5, boder: "solid 2px hsla(0, 0%, 0%, 1)"},
+    {scale:1, top:"1rem", left:"5rem", x: '50%', y: '50%', border: "solid 2px hsla(0, 0%, 0%, 0)", padding: "0"}
+    );
+
+tlN.fromTo('.nav', {background: "transparent", color: "white"}, {background:"white", color: "black"});
