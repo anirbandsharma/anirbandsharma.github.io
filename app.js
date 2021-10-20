0ef8c80fb925c68080bd4bac364129dc7f7d2be2
anirbandsharma <85803381+anirbandsharma@users.noreply.github.com>
2021-10-21 01:49:52 +0530

    update

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ let tlN = gsap.timeline({
     },
 });
 
+let tlText = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "80%",
+        scrub: 1,
+    },
+});
+
+let tlPhoto = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: "0%",
+        end: "80%",
+        scrub: 1,
+    },
+});
 
 let tl3 = gsap.timeline({
     scrollTrigger: {
@@ -39,10 +56,13 @@ let tl3 = gsap.timeline({
 
 tl.fromTo('.slide', {y:0}, {y:-400});
 
+tlText.fromTo('.text', {x:0}, {x:-800});
+tlPhoto.fromTo('.photo', {x:0}, {x:800});
+
 tl2.fromTo(
     '.logo', 
-    {scale:5, boder: "solid 2px hsla(0, 0%, 0%, 1)"},
-    {scale:1, top:"0.6rem", left:"5rem", x: '50%', y: '50%', border: "solid 2px hsla(0, 0%, 0%, 0)", padding: "0"}
+    {scale:4, boder: "solid 2px hsla(0, 0%, 0%, 1)"},
+    {scale:1, top:"0.6rem", left:"5rem", x: '50%', y: '50%'}
     );
 
-tlN.fromTo('.nav', {background: "transparent", color: "white"}, {background:"#ffffffea", color: "black"});
+//tlN.fromTo('.nav', {background: "transparent", color: "white"}, {background:"#ffffffea", color: "black"});
