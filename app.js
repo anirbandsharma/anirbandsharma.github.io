54ae605cfd5bbf9acf39f9b3d6bf7bb84ea41b68
anirbandsharma <85803381+anirbandsharma@users.noreply.github.com>
2021-10-23 01:08:55 +0530

    fixes

--- app.js
+++ app.js
@@ -4,50 +4,48 @@
 var myNav = document.getElementById('nav');
 var photo = document.getElementById('photo');
 var logo = document.getElementById('logo');
-var links = document.getElementById('links');
+
+var menuBtn = document.getElementById("menu");
+var menuOpen = document.getElementById("menu-open");
 
 window.onscroll = function () { 
     "use strict";
-        if (document.body.scrollTop >= 300 || document.documentElement.scrollTop >= 300 ) {
+        if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
             myNav.classList.add("nav-colored");
             myNav.classList.remove("nav-transparent");
 
-            photo.style.right = "-100%";
-            logo.style.left = "-100%";
-
-            links.style.color = "black";
-            links.style.textShadow = "none";
+            menuOpen.style.color = "black";
+            menuOpen.style.textShadow = "none";
         } 
         else {
             myNav.classList.add("nav-transparent");
             myNav.classList.remove("nav-colored");
 
-            photo.style.right = "0";
-            logo.style.left = "0";
-
-            links.style.color = "white";
-            links.style.textShadow = "0 0 3px rgb(0, 0, 0)";
+            menuOpen.style.color = "white";
+            menuOpen.style.textShadow = "0 0 3px rgb(0, 0, 0)";
         }
     } 
 
 
-var menuBtn = document.getElementById("menu");
-var menuOpen = document.getElementById("menu-open");
-
-menuBtn.onclick = function () {
+  menuBtn.onclick = function () {
     menuOpen.style.display = "flex";
     document.body.style.overflow = "hidden";
-}
+    document.getElementById("nav").style.padding = "0";
+  };
 
-window.onclick = function (event) {
+  window.onclick = function (event) {
     if (event.target == menuOpen) {
-        menuOpen.style.display = "none";
-        document.body.style.overflow = "auto";
+      menuOpen.style.display = "none";
+      document.body.style.overflow = "auto";
+      document.getElementById("nav").style.padding = "1rem 1rem";
     }
-}
+  };
 
-/* Close */
-function closeN() {
+  /* Close */
+  function closeN() {
+    if ($(window).width() < 600) {
     menuOpen.style.display = "none";
     document.body.style.overflow = "auto";
+    document.getElementById("nav").style.padding = "1rem 1rem";
+    }
   }
